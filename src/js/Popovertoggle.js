d73e828834cac7f02bea66a721e51d9db6e65f7c
unknown <kgeluza@mail.ru>
2022-02-15 20:51:07 +0400

    commit 15/02

---
+++ src/js/Popovertoggle.js
@@ -0,0 +1,14 @@
+// TODO: write your code here
+function clickPop() {
+  const btn = document.querySelector('.btn-popover');
+  const popover = document.querySelector('.popover');
+  btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    popover.classList.toggle('disactive');
+    btn.offsetParent.appendChild(popover);
+    popover.style.left = `${btn.offsetLeft - popover.offsetWidth / 8}px`;
+    popover.style.top = `${btn.offsetTop - popover.offsetHeight - btn.offsetHeight / 4}px`;
+  });
+}
+
+clickPop();
---
+++ src/js/e2e/Popovertoggle.test.js
@@ -0,0 +1,27 @@
+import puppetteer from 'puppeteer';
+
+jest.setTimeout(30000);
+describe('Popover Widget ', () => {
+  let browser = null;
+  let page = null;
+  const baseUrl = 'http://localhost:9000';
+  beforeAll(async () => {
+    browser = await puppetteer.launch({
+      // headless: true,
+      // slowMo: 100,
+      // devtools: false,
+    });
+    page = await browser.newPage();
+  });
+  afterAll(async () => {
+    await browser.close();
+  });
+  describe('Popover Click ', () => {
+    test('should show popover by click', async () => {
+      await page.goto(baseUrl);
+      const btn = await page.$('.btn-popover');
+      btn.click();
+      await page.waitForSelector('.popover');
+    });
+  });
+});
